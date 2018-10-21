22d2643bfac9fad3d0c020f57c9d0718297ae6eb
GhBogdan97 <bogdyg97@gmail.com>
2018-10-21 21:13:12 +0300

    schelet

---
+++ collective-project-backend/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace collective_project_backend.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
