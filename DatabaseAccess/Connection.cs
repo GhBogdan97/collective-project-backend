4c782857c5cfcbc62129007761cbb79f60d32782
GhBogdan97 <bogdyg97@gmail.com>
2018-11-11 22:55:41 +0200

    undo connection strings changes

--- DatabaseAccess/Connection.cs
+++ DatabaseAccess/Connection.cs
@@ -6,7 +6,7 @@
         {
             get
             {
-                return "Data Source=localhost\\SQLEXPRESS01;Initial Catalog=Internship1;Integrated Security=True;";
+                return "Data Source=DESKTOP-L6BBN0T\\SQLEXPRESS;Initial Catalog=Internship1;Integrated Security=True;";
             }
         }
     }
