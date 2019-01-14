629d704ced19d349e9d483edf470670b4a061ec0
simonaburlacu <simonaburlacu01@gmail.com>
2019-01-14 23:11:11 +0200

    register student

--- collective-project-backend/ViewModels/StudentViewModel.cs
+++ collective-project-backend/ViewModels/StudentViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,14 +10,23 @@ namespace API.ViewModels
 	public class StudentViewModel
 	{
         public int Id { get; set; }
-		public string Firstname { get; set; }
-		public string Lastname { get; set; }
-		public string University { get; set; }
+        [Required]
+        public string Firstname { get; set; }
+        [Required]
+        public string Lastname { get; set; }
+        [Required]
+        public string University { get; set; }
 		public string Specialization { get; set; }
 		public string College { get; set; }
 		public int Year { get; set; }
+        [Required]
 		public string Email { get; set; }
-		public string Password { get; set; }
-		public string ConfirmPassword { get; set; }
-	}
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        public IFormFile Cv { get; set; }
+
+    }
 }
